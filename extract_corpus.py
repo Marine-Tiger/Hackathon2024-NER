fe22790c3b6a958810d5920179e6507e7cedab0a
Erwan BOEHM <erwan.boehm@gmail.com>
2024-06-27 12:13:32 +0200

    explain code + add search engine in adjectives.ipynb

--- extract_corpus.py
+++ extract_corpus.py
@@ -11,11 +11,16 @@ class Chunk(pydantic.BaseModel):
     text: str
 
 def process_document(text: str, file_path: str, documents: dict):
-
+    # fonction qui prends un objet documents (type dict) et va le remplir de la facon suivante
+    # on associe a chaque annee une liste de Chunk (portion de texte) -> {annee: [Chunk, Chunk, Chunk]}
     current_year = 0
 
     seance_splitters= ["séance du", "comité du"]
 
+    # on parcours le document ligne par ligne
+    # si on tombe sur une ligne qui contient une nouvelle séance / comité avec une année
+    # on dit qu'a partir de maintenant toute les lignes suivantes concernent cette année (on met a jours current_year)
+    # jusqu'a ce qu'on tombe sur une nouvelle ligne avec une annee...
     for line in text.split('\n'):
         processed_line=line.strip().lower()
         if any([processed_line.startswith(seance_splitter) for seance_splitter in seance_splitters]) :
@@ -37,6 +42,7 @@ def process_document(text: str, file_path: str, documents: dict):
 def get_corpus(data_folder: str):
     documents = {}
 
+    # on applique la logique de process_document ()
     for filename in os.listdir(data_folder):
         file_path = os.path.join(data_folder, filename)
         if os.path.isfile(file_path): #On vérifie que c'est bien un fichier
@@ -48,6 +54,10 @@ def get_corpus(data_folder: str):
     return documents
 
 def get_coref_corpus():
+    # la on charge juste le fichier pickle cree par le notebook dataset_to_coref.ipynb
+    # pickle permet de sauvegarder n'immporte quel variable dans un fichier pckl et de le recharger.
+    # donc au lieu de relancer dataset_to_coref a chaque fois (long + pb de versions)
+    # je l'ai lance une fois, puis je l'ai stocke dans un coref_corpus.pckl
     import pickle
     file = open('../coref_corpus.pckl','rb')
     data = pickle.load(file)
